77140b297a48fe9a1830379fddf554515b2d6d58
sponkor <caleb.kostroun@outlook.com>
2025-01-11 04:35:24 -0600

    4: Updated letter counting, added comments

--- main.py
+++ main.py
@@ -1,24 +1,21 @@
 def main():
-    with open("./books/frankenstein.txt") as f:
-        bookContents = f.read()
-        #print(bookContents) prints the entirety of the text
-        #print(wordCount(bookContents)) prints the total word count
-        print(letterCount(bookContents))
+    with open("./books/frankenstein.txt") as f: #opens the file and assigns it to 'f'
+        bookContents = f.read() #formats the variable into a string and assigns it to 'bookContents'
+        print(letterCount(wordCount(bookContents)))
 
-def wordCount(text):
-    totalCount = []
-    totalCount = text.split()
-    return len(totalCount)
+def wordCount(text): #takes the contents of the text and returns the total number of words
+    return text.split()
 
-def letterCount(wordList):
-    allCharacters = {}
-    for item in wordList:
-        character = item.split()
-        for letter in character:
-            lowercase = letter.lower()
-            if lowercase.isalpha() == True and lowercase in allCharacters:
-                allCharacters[lowercase] += 1
-            elif lowercase.isalpha() and lowercase not in allCharacters:
-                allCharacters[lowercase] = 1
-    return allCharacters
+def letterCount(wordList): #takes a list of words and returns the count of each letter
+    allLetters, listIndex, value = [], {}, 0 #declaring variables that get manipulated by the function
+    for i in range(0, len(wordList)): 
+        splitChar = list(wordList[i].lower()) #takes each word, makes it lowercase and splits the word into a list
+        for letter in splitChar: #takes each character in a word and passes it to the 'if' function below which tests if each character is a letter and if it's been indexed 
+            if letter.isalpha() == True and letter in listIndex: #if the letter has already been indexed the "count" associated with the character is increased
+                allLetters[listIndex[letter]]["count"] += 1
+            elif letter.isalpha() and letter not in listIndex: #if the letter hasn't been indexed, it is added and given a value and appended the list of characters and their counts
+                listIndex[letter] = value
+                value += 1
+                allLetters.append({"letter": letter, "count": 1})
+    return allLetters
 main()
