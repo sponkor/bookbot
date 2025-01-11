de9664ed4034664a77a4e5cf77e9a8b908fa3329
sponkor <caleb.kostroun@outlook.com>
2025-01-11 02:42:52 -0600

    2: Added word count

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 def main():
     with open("./books/frankenstein.txt") as f:
         bookContents = f.read()
-        print(bookContents)
+        #print(bookContents)
+        print(wordCount(bookContents))
+
+def wordCount(text):
+    totalCount = []
+    totalCount = text.split()
+    return len(totalCount)
         
 main()
