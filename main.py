bceaa4222b46bd0d360ce455018142549c172d3a
sponkor <caleb.kostroun@outlook.com>
2025-01-11 02:54:31 -0600

    3: Added letter counting

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 def main():
     with open("./books/frankenstein.txt") as f:
         bookContents = f.read()
-        #print(bookContents)
-        print(wordCount(bookContents))
+        #print(bookContents) prints the entirety of the text
+        #print(wordCount(bookContents)) prints the total word count
+        print(letterCount(bookContents))
 
 def wordCount(text):
     totalCount = []
     totalCount = text.split()
     return len(totalCount)
-        
+
+def letterCount(wordList):
+    allCharacters = {}
+    for item in wordList:
+        character = item.split()
+        for letter in character:
+            lowercase = letter.lower()
+            if lowercase.isalpha() == True and lowercase in allCharacters:
+                allCharacters[lowercase] += 1
+            elif lowercase.isalpha() and lowercase not in allCharacters:
+                allCharacters[lowercase] = 1
+    return allCharacters
 main()
